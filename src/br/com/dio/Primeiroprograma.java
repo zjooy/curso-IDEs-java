dddcb01367cc7caeb7dfff80f9489b6a38f377e2
zjooy <zjoy_@outlook.com>
2022-07-07 12:18:49 -0300

    first commit IDE

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+import br.com.dio.model.Gato;
+
+public class Primeiroprograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*System.out.println("Hello, world!");*/
+
+	}
+
+}
